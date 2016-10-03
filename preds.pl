dda30139fa12b815a277ab31d7f047ef9ed72537
anotherik <ricardojotagoncalves@hotmail.com>
2016-10-03 14:48:41 +0100

    new file

--- preds.pl
+++ preds.pl
@@ -1,57 +1,49 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% 1. Create more facts about geography
-%% 2. Examples of natural language questions follow below
-%%    Your program needs to parse those questions 
-%%    and give correct answers. 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Facts about rivers.
-% ------------------
-
-river(amazon,[atlantic,brazil,peru]).
-river(euphrates,[persian_gulf,iraq,syria,turkey]).
-river(ganges,[indian_ocean,spain,india,china]).
-river(nile,[mediterranean,egypt,sudan,uganda]).
-river(yangtze,[pacific,china]).
-river(douro,[portugal,spain]).
-river(minho,[portugal,spain]).
-river(tejo,[portugal,espanha]).
-
-% Facts about countries.
+% PREDICADOS AUXILIARES
 % ---------------------
 
-% country(Country,Region,Latitude,Longitude,
-%         Area/1000,Area mod 1000,
-%         Population/1000000,Population mod 1000000 / 1000,
-%         Capital,Currency)
+% verifica se um dado item pertence a uma lista
+on(Item, [Item|_]).
+on(Item, [_|Tail]) :- on(Item, Tail).
 
-country(brazil,south_america,-14,-52,3286,470,200,400,brasilia,real).
-country(portugal,southern_europe,39,-8,35,340,10,460,lisbon,euro).
-country(spain,southern_europe,40,-4,504,782,46,64,madrid,euro).
+% calcula o valor maximo de uma lista de numeros
+max([X], X).
+max([X1,X2|Tail], Max) :- X1 > X2, max([X1|Tail], Max).
+max([X1,X2|Tail], Max) :- X1 =< X2, max([X2|Tail], Max).
 
+% concatena duas listas
+append([], L, L). 
+append([H|T], L1, [H|L2]) :- append(T, L1, L2).
 
-% Facts about Europe.
-% ------------------
+% PREDICADOS PRINCIPAIS
+% ---------------------
 
-borders(portugal,spain).
-borders(portugal,atlantic).
+% todos os rios
+rivers_in(world, Result) :- river(Result, _).
+% rios de um dado pais
+rivers_in(Country, Result) :- river(Result, List), on(Country, List).
 
-% Facts about cities.
-% ------------------
+% capital/regiao/area de um dado pais
+capital(Country, Result) :- country(Country,_,_,_,_,_,_,_,Result,_).
+region(Country, Result) :- country(Country,Result,_,_,_,_,_,_,_,_).
+area(Country, Result) :- country(Country,_,_,_,Result,_,_,_,_,_).
 
-% city(City,Country,Population - million)
+% devolve uma lista com todos os paises/areas
+all_countries(Result) :- findall(Country, country(Country,_,_,_,_,_,_,_,_,_), Result).
+all_areas(Result) :- findall(Area, country(_,_,_,_,Area,_,_,_,_,_), Result).
 
-city(porto,portugal,0.230).
-city(madrid,spain,3.165).
-city(lisbon,portugal,0.516).
+% devolve uma lista com as areas dos paises da lista dada (esta deu-me um nó cérebro. Mas consegui!)
+areas([X], [Result]) :- area(X, Result).
+areas([H|T], Result) :- 
+	area(H, Area1), 
+	areas(T, List),
+	append(List, [Area1], Result).
 
-% Examples of natural language questions
-
-% What rivers are in the world? rivers_in(world, X).
-% What is the largest country? biggest_country(all, X).
-% Where is the largest country? tendo a anterior, esta é fácil
-% Which countrys capital is London? capital(X, london).
-% Is there some ocean that does not border any country?
-% What are the continents which contain more than two cities whose population exceeds 1 million?
-% Which country bordering the Mediterranean borders a country that is bordered by a country whose population exceeds the population of India?
-
+% verifica se um pais tem mais area que outro
+bigger(Country1, Country2) :- area(Country1, A1), area(Country2, A2), A1 > A2.
+% calcula qual o pais com mais area de todos
+biggest_country(all, Result) :- all_countries(List), biggest_country(List, Result).
+% calcula qual o pais com mais area de entre os paises da lista dada
+biggest_country(List, Result) :- 
+	areas(List, Areas),
+	max(Areas, Max_area), 
+	country(Result,_,_,_,Max_area,_,_,_,_,_).

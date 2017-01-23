cd176d0afe8da297e50b567ae62bfb6957f0d1b2
anotherik <ricardojotagoncalves@hotmail.com>
2017-01-23 17:52:08 +0000

    other distros dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,15 @@
-echo "I will install some dependencies for debian unix users :)"
-sudo apt-get install python-pip
+printf "Hello, I will install some dependencies for you.\n"
+printf "Choose your distro:\n(1) Debian based\n(2) Red Hat based\n(3) Other\n"
+read choice
+
+case "$choice" in
+	"1") sudo apt-get install python-pip
+	;;
+	"2") sudo yum install python-pip
+	;;
+	"3") continue
+	;;
+esac
+
 sudo pip install selenium
-sudo pip install beautifulsoup
+sudo pip install bs4
